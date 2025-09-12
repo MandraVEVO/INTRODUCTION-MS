98c3a31868384272acdc6d6863a79c8967ae1f39
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-09-12 11:54:03 -0600

    first commit

---
+++ products-ms/src/product/product.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '../../generated/prisma';
+
+@Injectable()
+export class ProductService extends PrismaClient implements OnModuleInit{
+
+  private readonly logger = new Logger('ProductService');
+
+  onModuleInit() {
+      this.$connect();
+      this.logger.log('DB connected');
+  }
+  create(createProductDto: CreateProductDto) {
+    return 'This action adds a new product';
+  }
+
+  findAll() {
+    return `This action returns all product`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
